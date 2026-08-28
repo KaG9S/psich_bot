812e523d39d365a2826b5fa5f04fe1a128c0a1b7
Kakante vest <jokageorik@gmail.com>
2026-08-28 10:56:27 +0300

    some features

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import telebot
-from time import sleep, time
-from src import *
-from src.loger import log
-from src.decos import main
+from time import sleep, time, strftime
+from shutil import move
+from src import tinydb
+from src.loger import log, clear
+from src.decos import before as main_bot
+from src.decos import after as loader_bot
+
+timers = []
+timers.append( [time() + tinydb.save_int, tinydb.save_int, tinydb.comm] )
+timers.append( [time() + 60, 60, (tinydb.comm if tinydb.changes > 20 else ( lambda: log(2, "Not enouth changes to commit automaticaly") ))] )
+
+def tick():
+    main_bot.bot.polling()
+    for i in range(len(timers)):
+        if timers[i][0] > time():
+            timers[i][2]()
+            timers[i][0] += timers[i][1]
+            log(0, f"Timer {i} reset")
+
 
 if __name__ == "__main__":
     try:
+        log(0, "Started succesfully")
         while True:
-            main.bot.polling()
+            tick()
             sleep(0.5)
+    except KeyboardInterrupt:
+       exit(1) 
     except Exception as e:
+        at_time = strftime("%H:%M:%S-%m.%d.%Y")
         log(3, f"{type(e).__name__}: {str(e)}")
+        move("logfile.log", f"logs/logfile-{at_time}.log")
--- src/loger.py
+++ src/loger.py
@@ -3,15 +3,20 @@ import inspect
 import time
 
 def clear():
-    with open(log_file) as f:
-        f.write("")
-
-def log(level: int = 1, mes: str = "Something happaned") -> None:
+    at_time = time.strftime("%H:%M:%S %m.%d.%Y")
     caller_frame = inspect.currentframe().f_back
     module: str = inspect.currentframe().f_back.f_globals.get('__name__') if caller_frame else "unknown"
-    
-    level = ["DEBUG", "INFO", "WARNING", "ERROR"][level]
-    at_time = time.strftime("%H:%M:%S %m.%d.%Y")
+    with open(log_file) as f:
+        f.write("")
+        print(f"[{find_parent()}] [WARN] [{at_time}] : All logs cleared")
+        log(0, "Logs cleared, this is new log")
+
+def log(level: int = 1, mes: str = "Something happaned") -> None:
+    at_time = time.strftime(f"%H:%M:{time.time() % 60} %m.%d.%Y")
+    caller_frame = inspect.currentframe().f_back
+    module: str = inspect.currentframe().f_back.f_globals.get('__name__') if caller_frame else "unknown"
+    level = ["DEBUG", "INFO", "WARN", "ERROR"][level]
     final_str = f"[{module}] [{level}] [{at_time}] : {mes}"
     with open(log_file, 'a') as f:
         f.write(final_str+'\n')
+    print(final_str)
