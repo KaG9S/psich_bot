02b5a3db64051d0aa2e8833f03aa6f3def1f32e0
Kakante vest <jokageorik@gmail.com>
2026-08-28 10:49:56 +0300

    who tf is --global

--- src/decos/start.py
+++
@@ -1,5 +0,0 @@
-from src.decos.before import *
-
-@bot.message_handler(commands=['start'])
-def c_start(message):
-    send(message, "Привіт, радий тебе вітати! Що б отримати список дотупних команд введи /help")
